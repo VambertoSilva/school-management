6468ad6678a963ded848e0ef921f5b7219115954
Vamberto <vambertosilva.vas@gmail.com>
2025-03-31 15:22:20 -0300

    feat(database): added and update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,44 +1,25 @@
-UPDATE TABLE users (
+CREATE TABLE users (
     id UUID PRIMARY KEY ,
     username VARCHAR(255) UNIQUE ,
     password VARCHAR(255),
     email VARCHAR(100),
     name VARCHAR(255),
-    role VARCHAR(20) ,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    created_by UUID REFERENCES users(id),
-    updated_by UUID REFERENCES users(id)
+    role VARCHAR(20)
 );
-
 
 CREATE TABLE classification (
     id UUID PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    description TEXT
+    name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE book (
     id UUID PRIMARY KEY NOT NULL,
+    user_id UUID REFERENCES users(id),
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255),
     publish_date DATE,
     classification_id UUID REFERENCES classification(id) ON DELETE SET NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    created_by UUID REFERENCES users(id),
-    updated_by UUID REFERENCES users(id)
-);
-
-
-CREATE TABLE book_copy (
-    id UUID PRIMARY KEY NOT NULL,
-    book_id UUID REFERENCES book(id,
-    status VARCHAR(50) DEFAULT 'available',
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP ,
-    created_by UUID REFERENCES users(id),
-    updated_by UUID REFERENCES users(id)
+    status VARCHAR(50)
 );
 
 CREATE TABLE reservation (
@@ -46,11 +27,8 @@ CREATE TABLE reservation (
     user_id UUID REFERENCES users(id),
     book_id UUID REFERENCES book(id),
     reservation_date TIMESTAMP,
-    status VARCHAR(50),
-    created_at TIMESTAMP ,
-    updated_at TIMESTAMP
+    status VARCHAR(50)
 );
-
 
 CREATE TABLE loan (
     id UUID PRIMARY KEY NOT NULL,
@@ -62,9 +40,18 @@ CREATE TABLE loan (
     fine_amount DECIMAL(10, 2),
     fine_date TIMESTAMP,
     paid BOOLEAN,
-    renewal balance INTEGER,
-    status VARCHAR(50) ',
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    renewal_balance INTEGER,
+    status VARCHAR(50)
 );
+
+
+--CREATE TABLE book_copy (
+--    id UUID PRIMARY KEY NOT NULL,
+--    book_id UUID REFERENCES book(id),
+--    status VARCHAR(50) DEFAULT 'available',
+--    created_at TIMESTAMP,
+--    updated_at TIMESTAMP ,
+--    created_by UUID REFERENCES users(id),
+--    updated_by UUID REFERENCES users(id)
+--);
 
