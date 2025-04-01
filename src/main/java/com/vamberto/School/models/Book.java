3efe02407e80985a0df203ca749718d129ba3611
Vamberto <vambertosilva.vas@gmail.com>
2025-04-01 19:28:44 -0300

    refactor(database): update

--- src/main/java/com/vamberto/School/models/Book.java
+++ src/main/java/com/vamberto/School/models/Book.java
@@ -16,21 +16,20 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    private UUID userId;
-
     @Column(nullable = false, length = 255)
     private String title;
 
-    @Column(length = 255)
+    @Column(length = 255, nullable = false)
     private String author;
 
-    @Column(name = "publish_date")
+    @Column(name = "publish_date", nullable = false)
     private LocalDate publishDate;
 
 
     @Column(name = "classification_id")
     private UUID classificationId;
 
+    @Column(nullable = false, length = 40)
     private BookStatus status;
 
 }
