a8249c88c923cebfc4f2c77a7ae83f37c915da9d
Vamberto <vambertosilva.vas@gmail.com>
2025-04-01 19:30:34 -0300

    feat (database): Added loan service

---
+++ src/main/java/com/vamberto/School/controllers/LoanController.java
@@ -0,0 +1,32 @@
+package com.vamberto.School.controllers;
+
+import com.vamberto.School.dtos.LoanDTO;
+import com.vamberto.School.models.Loan;
+import com.vamberto.School.services.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/loan")
+@RequiredArgsConstructor
+public class LoanController {
+
+    private final LoanService loanService;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody LoanDTO DTO){
+
+        try{
+            Loan response = loanService.createLoan(DTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(Map.of("Erro: ", e.getMessage()));
+        }
+
+    }
+
+}
