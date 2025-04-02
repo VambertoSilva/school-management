e4841117bbba1fa9089ef3216653e9e395b184a0
Vamberto <vambertosilva.vas@gmail.com>
2025-04-02 13:28:47 -0300

    Feat (loan): added query

--- src/main/java/com/vamberto/School/repositories/LoanRepository.java
+++ src/main/java/com/vamberto/School/repositories/LoanRepository.java
@@ -1,9 +1,13 @@
 package com.vamberto.School.repositories;
 
 import com.vamberto.School.models.Loan;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface LoanRepository  extends JpaRepository<Loan, UUID> {
+    List<Loan>  findByReturnDate(LocalDate date);
 }
