ee54d0432b34ed11d9b0f43fceb0d8e68a8f8940
Vamberto <vambertosilva.vas@gmail.com>
2025-04-02 13:27:27 -0300

    Feat (loan): added exception

---
+++ src/main/java/com/vamberto/School/Exception/LoanRenewInvalidDate.java
@@ -0,0 +1,9 @@
+package com.vamberto.School.Exception;
+
+public class LoanRenewInvalidDate extends RuntimeException{
+
+    public LoanRenewInvalidDate(String message){
+        super(message);
+    }
+
+}
