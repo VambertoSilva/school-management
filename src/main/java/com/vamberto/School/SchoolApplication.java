f25489ecfee4da65059e6632b7affec063c0f6ca
Vamberto <vambertosilva.vas@gmail.com>
2025-04-02 13:31:24 -0300

    chore (Scheduling): added Scheduling

--- src/main/java/com/vamberto/School/SchoolApplication.java
+++ src/main/java/com/vamberto/School/SchoolApplication.java
@@ -3,8 +3,10 @@ package com.vamberto.School;
 import com.vamberto.School.configs.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchoolApplication {
 
 
