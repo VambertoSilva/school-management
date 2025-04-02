e51a45f34a4b7593155fd79471603c8472c42d58
Vamberto <vambertosilva.vas@gmail.com>
2025-04-01 23:00:28 -0300

    Feat (errors): added exception

---
+++ src/main/java/com/vamberto/School/Exception/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.vamberto.School.Exception;
+
+
+public class BookNotFoundException extends RuntimeException{
+
+    public  BookNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/vamberto/School/Exception/LoanNotFoundException.java
@@ -0,0 +1,10 @@
+package com.vamberto.School.Exception;
+
+public class LoanNotFoundException extends RuntimeException{
+
+
+    public LoanNotFoundException(String message){
+        super(message);
+    }
+
+}
