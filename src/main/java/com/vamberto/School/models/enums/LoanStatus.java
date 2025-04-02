9081482503dbca29114d28d710fbe900630eade6
Vamberto <vambertosilva.vas@gmail.com>
2025-04-02 18:24:52 -0300

    feat (database): added fine_paid

--- src/main/java/com/vamberto/School/models/enums/LoanStatus.java
+++ src/main/java/com/vamberto/School/models/enums/LoanStatus.java
@@ -1,10 +1,11 @@
 package com.vamberto.School.models.enums;
 
 public enum LoanStatus {
-    ACTIVE,         // Empréstimo ativo
-    OVERDUE,        // Atrasado (não devolvido no prazo)
+    ACTIVE,         // Empréstimo ativo **
+    OVERDUE,        // Atrasado (não devolvido no prazo ) **
     RETURNED,       // Livro devolvido no prazo
     LATE_RETURNED,  // Livro devolvido com atraso
+    FINE_PAID,      // multa paga
     LOST,           // Livro perdido
     LOST_NO_PENALTY // Livro perdido sem penalidades ao usuario
 }
