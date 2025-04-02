00081eda51e34754012cc9c09419849c4fabe197
Vamberto <vambertosilva.vas@gmail.com>
2025-04-02 20:13:39 -0300

    feat (loan): added restriction to book borrowed

--- src/main/java/com/vamberto/School/repositories/LoanRepository.java
+++ src/main/java/com/vamberto/School/repositories/LoanRepository.java
@@ -12,6 +12,6 @@ import java.util.UUID;
 
 public interface LoanRepository  extends JpaRepository<Loan, UUID> {
     List<Loan>  findByReturnDate(LocalDate date);
-    @Query(value = "SELECT COUNT(*) FROM loan WHERE user_id = :userId AND status IN ('0','1')", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM loan WHERE user_id = :userId AND status IN ('0','3')", nativeQuery = true)
     int countActiveOrOverdueLoans(@Param("userId") UUID userId);
 }
