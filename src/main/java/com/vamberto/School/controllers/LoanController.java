24a5ebf7015b917256b9d94e9fa6c5688af9f811
Vamberto <vambertosilva.vas@gmail.com>
2025-04-01 23:01:45 -0300

    Feat (loan): added loan management

--- src/main/java/com/vamberto/School/controllers/LoanController.java
+++ src/main/java/com/vamberto/School/controllers/LoanController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,6 +27,23 @@ public class LoanController {
         }catch (RuntimeException e){
             return ResponseEntity.badRequest().body(Map.of("Erro: ", e.getMessage()));
         }
+
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<Loan> returnLoan(@PathVariable String id){
+
+        Loan reponse = loanService.returnLoan(id);
+
+        return ResponseEntity.ok(reponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Loan>> listLoan(){
+
+        List<Loan>  listLoan = loanService.listLoan();
+
+        return ResponseEntity.ok(listLoan);
 
     }
 
--- src/main/java/com/vamberto/School/services/LoanService.java
+++ src/main/java/com/vamberto/School/services/LoanService.java
@@ -1,5 +1,7 @@
 package com.vamberto.School.services;
 
+import com.vamberto.School.Exception.BookNotFoundException;
+import com.vamberto.School.Exception.LoanNotFoundException;
 import com.vamberto.School.dtos.LoanDTO;
 import com.vamberto.School.models.Loan;
 import com.vamberto.School.models.enums.LoanStatus;
@@ -11,6 +13,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequestMapping
@@ -23,18 +28,40 @@ public class LoanService {
 
     public Loan createLoan(LoanDTO dto){
 
-        if(bookRepository.existsById(dto.bookId()) && usersRepository.existsById(dto.userId())){
-            Loan newLoan = new Loan();
-            newLoan.setLoanDate(LocalDateTime.now());
-            newLoan.setStatus(LoanStatus.ACTIVE);
-            newLoan.setUserId(dto.userId());
-            newLoan.setBookId(dto.bookId());
-            newLoan.setReturnDate(LocalDateTime.now().plusWeeks(1));
 
-            return  loanRepository.save(newLoan);
+            if (bookRepository.existsById(dto.bookId()) && usersRepository.existsById(dto.userId())) {
+                Loan newLoan = new Loan();
+                newLoan.setLoanDate(LocalDateTime.now());
+                newLoan.setStatus(LoanStatus.ACTIVE);
+                newLoan.setUserId(dto.userId());
+                newLoan.setBookId(dto.bookId());
+                newLoan.setReturnDate(LocalDateTime.now().plusWeeks(1));
+
+                return loanRepository.save(newLoan);
+            } else {
+                throw new RuntimeException("Erro ao verificar os dados");
+            }
+
+
+    }
+
+    public Loan returnLoan(String id){
+        UUID uuidID = UUID.fromString(id);
+        Optional<Loan> loanOptional = loanRepository.findById(uuidID);
+
+        if(loanOptional.isPresent()){
+            Loan loan = loanOptional.get();
+            loan.setStatus(LoanStatus.RETURNED);
+            return loanRepository.save(loan);
+
         }else{
-            throw new RuntimeException("Erro ao verificar os dados");
+            throw new LoanNotFoundException("Loan not found");
         }
 
+
+    }
+
+    public List<Loan> listLoan(){
+        return  loanRepository.findAll();
     }
 }
