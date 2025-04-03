365ec6f8a4783804836f511d743f9b0567d92667
Vamberto <vambertosilva.vas@gmail.com>
2025-04-03 18:28:39 -0300

    feat (reservation): added reservation

--- src/main/java/com/vamberto/School/repositories/ReservationRepository.java
+++ src/main/java/com/vamberto/School/repositories/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.vamberto.School.repositories;
 
 import com.vamberto.School.models.Reservation;
+import com.vamberto.School.models.enums.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReservationRepository  extends JpaRepository<Reservation, UUID> {
+Optional<Reservation> findByBookIdAndStatus(UUID bookId, ReservationStatus status);
 }
