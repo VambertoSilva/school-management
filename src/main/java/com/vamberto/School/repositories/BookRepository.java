fe516078ca38ba894cb8ca853212ce749ee15f21
Vamberto <vambertosilva.vas@gmail.com>
2025-04-03 18:29:32 -0300

    feat (reservation): added FilterPage

--- src/main/java/com/vamberto/School/repositories/BookRepository.java
+++ src/main/java/com/vamberto/School/repositories/BookRepository.java
@@ -1,16 +1,10 @@
 package com.vamberto.School.repositories;
 
 import com.vamberto.School.models.Book;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-
-import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
