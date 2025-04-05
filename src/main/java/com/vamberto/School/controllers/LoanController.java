b2d984c708ed643fbca5d362b373636a54cf23a1
Vamberto <vambertosilva.vas@gmail.com>
2025-04-04 21:22:19 -0300

    feat (loan) added pagination

--- src/main/java/com/vamberto/School/controllers/LoanController.java
+++ src/main/java/com/vamberto/School/controllers/LoanController.java
@@ -3,9 +3,13 @@ package com.vamberto.School.controllers;
 import com.vamberto.School.Exception.LoanNotFoundException;
 import com.vamberto.School.Exception.LoanRenewInvalidDate;
 import com.vamberto.School.dtos.LoanDTO;
+import com.vamberto.School.models.Book;
 import com.vamberto.School.models.Loan;
+import com.vamberto.School.models.enums.BookStatus;
+import com.vamberto.School.models.enums.LoanStatus;
 import com.vamberto.School.services.LoanService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,9 +45,14 @@ public class LoanController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Loan>> listLoan(){
+    public ResponseEntity<Page<Loan>> listLoan(@RequestParam(defaultValue = "") String title,
+                                               @RequestParam(defaultValue = "0") int page,
+                                               @RequestParam(defaultValue = "5") int size,
+                                               @RequestParam(defaultValue = "title") String sortBy,
+                                               @RequestParam(defaultValue = "asc") String direction,
+                                               @RequestParam(defaultValue = "ALL") LoanStatus filter){
 
-        List<Loan>  listLoan = loanService.listLoan();
+        Page<Loan> listLoan = loanService.listLoan(title, page, size, sortBy, direction, filter);
 
         return ResponseEntity.ok(listLoan);
 
