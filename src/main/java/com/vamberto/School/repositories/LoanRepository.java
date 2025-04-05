37a406ec8070d6e2d092a3ae66ff2b13f9b785bb
Vamberto <vambertosilva.vas@gmail.com>
2025-04-04 21:22:24 -0300

    feat (loan) added pagination

--- src/main/java/com/vamberto/School/repositories/LoanRepository.java
+++ src/main/java/com/vamberto/School/repositories/LoanRepository.java
@@ -1,6 +1,8 @@
 package com.vamberto.School.repositories;
 
 import com.vamberto.School.models.Loan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,7 @@ public interface LoanRepository  extends JpaRepository<Loan, UUID> {
     List<Loan>  findByReturnDate(LocalDate date);
     @Query(value = "SELECT COUNT(*) FROM loan WHERE user_id = :userId AND status IN ('0','3')", nativeQuery = true)
     int countActiveOrOverdueLoans(@Param("userId") UUID userId);
+
+    Page<Loan> findAll(Pageable pageable);
+    Page<Loan> findByTitleContainingIgnoreCase(String title,Pageable pageable);
 }
