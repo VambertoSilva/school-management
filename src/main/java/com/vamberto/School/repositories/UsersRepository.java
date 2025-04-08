5f370cae53fb5da6b4feccd2244f320ba5a835d5
Vamberto <vambertosilva.vas@gmail.com>
2025-04-07 21:49:21 -0300

    feat (security) added security

--- src/main/java/com/vamberto/School/repositories/UsersRepository.java
+++ src/main/java/com/vamberto/School/repositories/UsersRepository.java
@@ -3,7 +3,9 @@ package com.vamberto.School.repositories;
 import com.vamberto.School.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<Users, UUID> {
+    Optional<Users> findByUsername(String login);
 }
