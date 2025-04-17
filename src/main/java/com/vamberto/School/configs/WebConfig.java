c04190db3c908014052f12e3b64236db3eb018a6
Vamberto <vambertosilva.vas@gmail.com>
2025-04-16 22:27:41 -0300

    feat (security) added security

--- src/main/java/com/vamberto/School/configs/WebConfig.java
+++ src/main/java/com/vamberto/School/configs/WebConfig.java
@@ -1,19 +1,26 @@
 package com.vamberto.School.configs;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig {
+
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "") // Permitir o frontend
+                        .allowedOrigins("http://localhost:3000", "http://192.168.0.104:3000", "https://library.vamberto.com.br/" ) // Permitir o frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
